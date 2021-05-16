9e04754d49db2808fbfeeb32e8da83fd388141e9
José Gilberto Trujillo Melián <jose.hoc@gmail.com>
2021-05-17 00:57:44 +0100

    section 9, reflection

--- grpc-course-my-code/calculator/calculator_server/server.go
+++ grpc-course-my-code/calculator/calculator_server/server.go
@@ -14,6 +14,7 @@ import (
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/reflection"
 	"google.golang.org/grpc/status"
 )
 
@@ -130,6 +131,9 @@ func main() {
 
 	s := grpc.NewServer()
 	calculatorpb.RegisterCalculatorServiceServer(s, &server{})
+
+	//Register Reflection
+	reflection.Register(s)
 
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
